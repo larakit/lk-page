60888556b40bbfb1bdc9bb9e5e0f071cdda8eba8
Alexey Berdnikov <aberdnikov@gmail.com>
2016-04-28 09:12:15 +0500

    fixed conflict

--- src/init.php
+++ src/init.php
@@ -1,14 +1,5 @@
 <?php
 /*################################################################################
 //регистрируем сервис-провайдер
-<<<<<<< HEAD
 ################################################################################*/
-\Larakit\SPA::register_provider(Larakit\Page\LarakitServiceProvider::class);
-
-/*################################################################################
-  middlewares
-################################################################################*/
-\Larakit\SPA::register_middleware(\Larakit\Page\PageMiddlewareAfter::class);
-=======
 Larakit\SPA::register_provider(Larakit\Page\LarakitServiceProvider::class);
->>>>>>> 5a05a971d166dbfb14cbbe4a0290780c1ff30c3a
