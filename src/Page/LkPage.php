309b9e3990be9bf2a621a3cfc96c8e1480d9c561
Бердников Алексей <berdnikov_ay@bunkermedia.ru>
2018-07-18 15:30:18 +0500

    fixed

--- src/Page/LkPage.php
+++ src/Page/LkPage.php
@@ -17,7 +17,7 @@ use Larakit\StaticFiles\Css;
 use Larakit\StaticFiles\Js;
 
 class LkPage {
-    
+
     protected $html;
     protected $head = [
         'viewport'     => 'width=device-width, initial-scale=1.0',
@@ -31,29 +31,29 @@ class LkPage {
     ];
     protected $body;
     static    $instance;
-    
+
     static function instance() {
-        if(!isset(self::$instance)) {
+        if (!isset(self::$instance)) {
             self::$instance = new LkPage();
         }
-        
+
         return self::$instance;
     }
-    
+
     function __construct() {
         $this->html = new Html();
         $this->html->setAttribute('lang', \App::getLocale());
         $this->body = new Body();
     }
-    
+
     function body() {
         return $this->body;
     }
-    
+
     function html() {
         return $this->html;
     }
-    
+
     /**
      * Получить список доступных для выбора тем оформления
      *
@@ -62,10 +62,10 @@ class LkPage {
     function getThemes() {
         $themes = env('PAGE_THEMES');
         $themes = explode(' ', $themes);
-        
+
         return $themes;
     }
-    
+
     /**
      * Установить текущую тему оформления
      *
@@ -75,14 +75,14 @@ class LkPage {
      */
     function setTheme($theme) {
         $themes = $this->getThemes();
-        foreach($themes as $_theme) {
+        foreach ($themes as $_theme) {
             $this->body->removeClass('theme--' . $_theme);
         }
         $this->body->addClass('theme--' . $theme);
-        
+
         return $this;
     }
-    
+
     /**
      * Задать содержимое страницы
      *
@@ -90,18 +90,18 @@ class LkPage {
      */
     function setBodyContent($content) {
         $this->body->setContent($content);
-        
+
         return $this;
     }
-    
+
     protected function setHead($k, $v) {
         $k              = mb_substr($k, 7);
         $k              = Str::snake($k);
         $this->head[$k] = $v;
-        
+
         return $this;
     }
-    
+
     /**
      * Установка заголовка страницы
      *
@@ -111,9 +111,9 @@ class LkPage {
      */
     function setHeadTitle($v) {
         return $this->setHead(__FUNCTION__, $v);
-        
+
     }
-    
+
     /**
      * Установка вьюпорта
      *
@@ -124,7 +124,7 @@ class LkPage {
     function setHeadViewport($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * Установка кейвордов
      *
@@ -135,7 +135,7 @@ class LkPage {
     function setHeadKeywords($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * Установка описания страницы
      *
@@ -146,18 +146,18 @@ class LkPage {
     function setHeadDescription($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     function setHeadDnsPrefetch($url) {
         $url                              = parse_url($url, PHP_URL_HOST);
         $this->head['dns_prefetch'][$url] = '//' . $url;
-        
+
         return $this;
     }
-    
+
     function setHeadRobots($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * разрешено индексировать текст и ссылки на странице, аналогично <meta name="robots" content="index, follow"/>
      *
@@ -166,7 +166,7 @@ class LkPage {
     function setHeadRobotsAll() {
         return $this->setHeadRobots('all');
     }
-    
+
     /**
      * не индексировать текст страницы,
      *
@@ -175,7 +175,7 @@ class LkPage {
     function setHeadRobotsNoIndex() {
         return $this->setHeadRobots('noindex');
     }
-    
+
     /**
      *  не переходить по ссылкам на странице,
      *
@@ -184,7 +184,7 @@ class LkPage {
     function setHeadRobotsNoFollow() {
         return $this->setHeadRobots('nofollow');
     }
-    
+
     /**
      * запрещено индексировать текст и переходить по ссылкам на странице, аналогично <meta name="robots" content="noindex, nofollow"/>
      *
@@ -193,7 +193,7 @@ class LkPage {
     function setHeadRobotsNone() {
         return $this->setHeadRobots('none');
     }
-    
+
     /**
      *  не показывать ссылку на сохраненную копию на странице результатов поиска.
      *
@@ -202,7 +202,7 @@ class LkPage {
     function setHeadRobotsNoArchive() {
         return $this->setHeadRobots('noarchive');
     }
-    
+
     /**
      * не использовать описание из Яндекс.Каталога для сниппета в результатах поиска.
      *
@@ -211,7 +211,7 @@ class LkPage {
     function setHeadRobotsNoYaca() {
         return $this->setHeadRobots('noyaca');
     }
-    
+
     /**
      * Установка режима совместимости
      *
@@ -222,11 +222,11 @@ class LkPage {
     function setHeadCompatible($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     function setHeadVideo($url, $w = null, $h = null) {
         return $this->setHead(__FUNCTION__, compact('url', 'w', 'h'));
     }
-    
+
     /**
      * название сайта, на котором размещена информация об объекте.
      *
@@ -237,7 +237,7 @@ class LkPage {
     function setHeadSiteName($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * URL изображения, описывающего объект (соответствует og:image).
      *
@@ -248,39 +248,43 @@ class LkPage {
     function setHeadImage($url, $w = null, $h = null) {
         $host   = parse_url($url, PHP_URL_HOST);
         $scheme = parse_url($url, PHP_URL_SCHEME);
-        if(!$host) {
+        if (!$host) {
             $host = \Request::getHost();
         }
-        if(!$scheme) {
+        if (!$scheme) {
             $scheme = \Request::getScheme();
         }
         $path                      = parse_url($url, PHP_URL_PATH);
         $url                       = $scheme . '://' . $host . $path;
         $this->head['image'][$url] = compact('url', 'w', 'h');
-        
+
         return $this;
     }
-    
+
     function setHeadRss($url) {
         $this->head['rss'][$url] = $url;
-        
+
         return $this;
     }
-    
+
     function setHeadBase($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
+    function setHeadSitemap($v) {
+        return $this->setHead(__FUNCTION__, $v);
+    }
+
     function setHeadFavicon($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     function setHeadAppend($v) {
         $this->head['append'][$v] = $v;
-        
+
         return $this;
     }
-    
+
     /**
      * Указывает кодировку документа.
      *
@@ -291,7 +295,7 @@ class LkPage {
     function setHeadCharset($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * @param $v
      *
@@ -300,7 +304,7 @@ class LkPage {
     function setHeadAuthor($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     /**
      * @param $v
      *
@@ -309,135 +313,141 @@ class LkPage {
     function setHeadCopyright($v) {
         return $this->setHead(__FUNCTION__, $v);
     }
-    
+
     function __toString() {
-        $this->html()->setAttribute('_token', csrf_token());
+        $this->html()
+             ->setAttribute('_token', csrf_token());
         //HEAD
-        $head   = [];
-        $title  = isset($this->head['title']) ? $this->head['title'] : \Request::getHost();
-        $head[] = '<!-- title -->';
-        $head[] = '<title>' . $title . '</title>';
-        $head[] = '<meta property="og:title" content="' . $title . '"/>';
-        $head[] = '';
+        $head      = [];
+        $title     = isset($this->head['title']) ? $this->head['title'] : \Request::getHost();
+        $head[]    = '<!-- title -->';
+        $head[]    = '<title>' . $title . '</title>';
+        $head[]    = '<meta property="og:title" content="' . $title . '"/>';
+        $head[]    = '';
         $site_name = isset($this->head['site_name']) ? $this->head['site_name'] : \Request::getHost();
         $head[]    = '<!-- site_name -->';
         $head[]    = '<meta property="og:site_name" content="' . $site_name . '">';
-        
-        $head[]    = '';
-        
-        foreach($this->head as $k => $v) {
-            if('base' == $k) {
+
+        $head[] = '';
+
+        foreach ($this->head as $k => $v) {
+            if ('sitemap' == $k) {
+                $head[] = '<!-- sitemap -->';
+                $head[] = '<link rel="sitemap" type="application/xml" title="Sitemap" href="' . $v . '" />';
+                $head[] = '';
+            }
+            if ('base' == $k) {
                 $head[] = '<!-- base -->';
                 $head[] = '<base href="' . $v . '" />';
                 $head[] = '';
             }
-            if('charset' == $k) {
+            if ('charset' == $k) {
                 $head[] = '<!-- charset -->';
                 $head[] = '<meta charset="' . $v . '" />';
                 $head[] = '<meta http-equiv="Content-Type" content="text/html; charset=' . $v . '" />';
                 $head[] = '';
             }
-            if('author' == $k) {
+            if ('author' == $k) {
                 $head[] = '<!-- author -->';
                 $head[] = '<meta name="author" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('copyright' == $k) {
+            if ('copyright' == $k) {
                 $head[] = '<!-- copyright -->';
                 $head[] = '<meta name="copyright" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('favicon' == $k) {
+            if ('favicon' == $k) {
                 $head[] = '<!-- favicon -->';
                 $head[] = '<link rel="icon" href="' . $v . '">';
                 $head[] = '';
             }
-            if('viewport' == $k) {
+            if ('viewport' == $k) {
                 $head[] = '<!-- viewport -->';
                 $head[] = '<meta name="viewport" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('generator' == $k) {
+            if ('generator' == $k) {
                 $head[] = '<!-- generator -->';
                 $head[] = '<meta name="generator" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('image' == $k) {
+            if ('image' == $k) {
                 $i = 0;
-                foreach($v as $value) {
+                foreach ($v as $value) {
                     $i++;
                     $head[] = '<!-- image (' . $i . ') -->';
                     $url    = Arr::get($value, 'url');
                     $w      = Arr::get($value, 'w');
                     $h      = Arr::get($value, 'h');
                     $head[] = '<meta property="og:image" content="' . $url . '">';
-                    if($w) {
+                    if ($w) {
                         $head[] = '<meta property="og:image:width" content="' . $w . '">';
                     }
-                    if($h) {
+                    if ($h) {
                         $head[] = '<meta property="og:image:height" content="' . $h . '">';
                     }
                     $head[] = '';
                 }
             }
-            if('video' == $k) {
+            if ('video' == $k) {
                 $head[] = '<!-- video -->';
                 $url    = Arr::get($v, 'url');
                 $w      = Arr::get($v, 'w');
                 $h      = Arr::get($v, 'h');
                 $head[] = '<meta property="og:video" content="' . $url . '">';
-                if($w) {
+                if ($w) {
                     $head[] = '<meta property="og:video:width" content="' . $w . '">';
                 }
-                if($h) {
+                if ($h) {
                     $head[] = '<meta property="og:video:height" content="' . $h . '">';
                 }
                 $head[] = '';
             }
-            if('rss' == $k) {
-                if(count($v)) {
+            if ('rss' == $k) {
+                if (count($v)) {
                     $head[] = '<!-- rss -->';
-                    foreach($v as $value) {
+                    foreach ($v as $value) {
                         $head[] = '<link rel="alternate" type="application/rss+xml" href="' . $value . '" />';
                     }
                     $head[] = '';
                 }
             }
-            if('append' == $k) {
+            if ('append' == $k) {
                 $head[] = '<!-- append -->';
-                foreach($v as $value) {
+                foreach ($v as $value) {
                     $head[] = $value;
                 }
                 $head[] = '';
             }
-            if('keywords' == $k) {
+            if ('keywords' == $k) {
                 $head[] = '<!-- keywords -->';
                 $head[] = '<meta name="og:keywords" content="' . $v . '" />';
                 $head[] = '<meta name="twitter:keywords" content="' . $v . '" />';
                 $head[] = '<meta name="keywords" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('description' == $k) {
+            if ('description' == $k) {
                 $head[] = '<!-- description -->';
                 $head[] = '<meta name="og:description" content="' . $v . '" />';
                 $head[] = '<meta name="twitter:description" content="' . $v . '" />';
                 $head[] = '<meta name="description" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('compatible' == $k) {
+            if ('compatible' == $k) {
                 $head[] = '<!-- compatible -->';
                 $head[] = '<meta http-equiv="X-UA-Compatible" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('robots' == $k) {
+            if ('robots' == $k) {
                 $head[] = '<!-- robots -->';
                 $head[] = '<meta name="robots" content="' . $v . '" />';
                 $head[] = '';
             }
-            if('dns_prefetch' == $k) {
-                if(count($v)) {
+            if ('dns_prefetch' == $k) {
+                if (count($v)) {
                     $head[] = '<!-- dns-prefetch" -->';
-                    foreach($v as $value) {
+                    foreach ($v as $value) {
                         $head[] = '<link rel="dns-prefetch" href="' . $value . '">';
                     }
                     $head[] = '';
@@ -452,16 +462,16 @@ class LkPage {
         $head[] = '';
         $head[] = '<!-- css" -->';
         $head   = '<head>' . PHP_EOL . "\t\t" . implode(PHP_EOL . "\t\t", $head) . PHP_EOL;
-        $head .= Css::instance();
-        $head .= '</head>';
-        
+        $head   .= Css::instance();
+        $head   .= '</head>';
+
         //BODY
         $content = $this->body->getContent();
         $this->body->setContent(PHP_EOL . $content . PHP_EOL . Js::instance() . PHP_EOL);
-        
+
         //HTML
         $this->html->setContent(PHP_EOL . $head . PHP_EOL . $this->body . PHP_EOL);
-        
+
         return '<!DOCTYPE html>' . PHP_EOL . $this->html;
     }
 }
